77bbea82f562cf409ceff40c1a1f6767942d76cb
Pietro Filla <pietrofilla@gmail.com>
2026-03-20 15:28:34 -0300

    feat: corrigi o codigo da api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-// 🔹 GET /api/me
+
 app.get('/api/me', (req, res) => {
     res.json({
         nome: "Pietro",
@@ -11,21 +11,21 @@ app.get('/api/me', (req, res) => {
     });
 });
 
-// 🔹 GET /api/data
+
 app.get('/api/data', (req, res) => {
     res.json({
         dataHora: new Date()
     });
 });
 
-// 🔹 GET /api/random
+
 app.get('/api/random', (req, res) => {
     res.json({
         numero: Math.floor(Math.random() * 100)
     });
 });
 
-// 🚀 Servidor
+
 app.listen(3000, () => {
     console.log("API 'api-eu' rodando em http://localhost:3000");
 });
